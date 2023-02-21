5d05471b9dfe3ff9c83a85242284c5556434e0a4
Niveen Abdelaatty <niveen.abdelaatty@gmail.com>
2023-02-21 15:25:40 -0500

    domMenuLab

---
+++ domMenuLab/index.js
@@ -0,0 +1,30 @@
+// Menu data structure
+var menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
+
+const mainEl = document.querySelector('main');
+
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+
+mainEl.classList.add('flex-ctr');
+
+const topMenuEl = document.getElementById('top-menu');
+
+topMenuEl.style.height = '100%';
+
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+topMenuEl.classList.add('flex-around');
+
+menuLinks.forEach((link)=>{
+    const a = document.createElement('a');
+    a.setAttribute('href', link.href);
+    a.textContent = link.text;
+    topMenuEl.appendChild(a);
+})
